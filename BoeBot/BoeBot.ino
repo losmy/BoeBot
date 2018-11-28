5c529127446318898a2e072ab1b5a400ce15530a
Jonathan <jonakes@student.chalmers.se>
2018-11-28 15:27:52 +0100

    Work in Progress

--- BoeBot/BoeBot.ino
+++ BoeBot/BoeBot.ino
@@ -1,13 +1,14 @@
 #include<Servo.h>
 
 const int pingPin = 7;
+const int turretPin=12;
 Servo turret;
 int i=0;
 boolean f=true;
 
 void setup(){
-pinMode(12, OUTPUT);
-turret.attach(12);
+pinMode(turretPin, OUTPUT);
+turret.attach(turretPin);
 Serial.begin(9600);
 }
 
